df8ceec2df5209710125521a994e09a06f4dfa1d
unknown <tohhongaik1018@gmail.com>
2022-03-14 10:49:12 +0800

    Update

--- ProgrammingUI/src/hongaik.java
+++ ProgrammingUI/src/hongaik.java
@@ -6,14 +6,30 @@ import java.text.NumberFormat;
 public class hongaik {
 	public static JPanel newPanel() { 
 		
-		JPanel panel = new JPanel(); 
-		panel.setPreferredSize(new Dimension(1050, 625));
-		panel.setBackground(Color.decode("#004F8A"));
-		panel.setLayout(new FlowLayout());
+ImageIcon TitleIcon = new ImageIcon("Beach.jpg");
 		
-		JLabel myLabel = new JLabel("Write your code  here, Hong Aik");
-		panel.add(myLabel);
+		JPanel panel = new JPanel(); 
+		panel.setBorder(BorderFactory.createMatteBorder(4, 4, 4, 4,Color.decode("#000000")));
+		panel.setPreferredSize(new Dimension(1050, 625));
+		panel.setBackground(main.lightgreen);
+		panel.setLayout(new GridLayout(4, 1, 1, 1));
+		
+		JLabel Title = new JLabel();
+		Title.setIcon(TitleIcon);
+		Title.setText("Welcome to Life Below Water Site");
+		Title.setVerticalAlignment(JLabel.TOP);
+		
 		//write your code below 
+		JPanel Intro = new JPanel();
+		JPanel Selection = new JPanel();
+		
+		//Creation of Title/Picture pane
+		Intro.setBackground(Color.decode("#09C3DB"));
+		Intro.setBounds(0,0,300,300);
+		Intro.setLayout(new FlowLayout());
+		Intro.add(Title);
+		panel.add(Intro);
+
 		
 		//write your code above
 		
