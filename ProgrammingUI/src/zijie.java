dd1bd35d9bd6c731bdcb3a1dfbc35cb173b74e1b
abc <abc@example.com>
2022-03-16 09:34:38 +0800

    21063409: ZJ Section Card

--- ProgrammingUI/src/zijie.java
+++ ProgrammingUI/src/zijie.java
@@ -16,11 +16,9 @@ public class zijie
 		
 		// Image for Banner
 		ImageIcon image = new ImageIcon("ProgrammingUI/src/zjBanner.jpg");
-
-		// Creation of Banner
-		//JPanel banner = new JPanel();
-		//banner.setBackground(Color.decode("#022902"));
-		//banner.setPreferredSize(new Dimension(1050, 225));
+		
+		// Inserting image into banner
+		JLabel zjBanner = new JLabel(image);
 		
 		// Creation of Information Panel
 		JPanel infoPanel = new JPanel();
@@ -37,7 +35,6 @@ public class zijie
 		Reason.setBackground(Color.decode("#ddefe3"));
 		Reason.add(ReasonText);
 		
-		
 		// Inserting text into Information Panel
 		JLabel information = new JLabel();
 		information.setText(("<html><br/>Trees contribute to clean the air we breathe,<br/>filter the water we drink, and support over<br/>"
@@ -48,17 +45,85 @@ public class zijie
 		infoPanel.add(Reason, BorderLayout.NORTH);
 		infoPanel.add(information);
 		
-		// Inserting image into banner
-		JLabel zjBanner = new JLabel(image);
+		// Creation of DonationPanel
+		JPanel DonationPanel = new JPanel();
+		DonationPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 300, 0));
+		DonationPanel.setBackground(Color.decode("#ddefe3"));
+		DonationPanel.setPreferredSize(new Dimension(1000, 300));
 		
-		//banner.add(zjBanner, BorderLayout.NORTH);
+		// Creation of panel for donation
+		JPanel plantingTrees = zijie.plantTree("Plant your tree today!", "ProgrammingUI/src/PlantingTree.jpg");
 		
-		// Adding the banner into the Main Panel
+		DonationPanel.add(plantingTrees);
+		
+		// Creation of panel for Donor List
+		JPanel Donors = new JPanel();
+		Donors.setPreferredSize(new Dimension(300, 300));
+		Donors.setBackground(Color.decode("#ddefe3"));
+		
+		JLabel DonorList = new JLabel();
+		
+		Donors.add(DonorList, BorderLayout.CENTER);
+		
+		// Adding the panels into the Main Panel
 		panel.add(zjBanner, BorderLayout.NORTH);
 		panel.add(infoPanel, BorderLayout.WEST);
+		panel.add(DonationPanel);
+		panel.add(Donors, BorderLayout.EAST);
 		
 		//write your code above
 		
 		return panel;
 	}
+	
+	public static JPanel plantTree(String desc, String imgSrc) {
+		
+		// Creation of the main Donation Panel
+		JPanel donationPanel = new JPanel();
+		donationPanel.setPreferredSize(new Dimension(225, 300));
+		donationPanel.setLayout(new BorderLayout(0, 0));
+		
+		// Creation of panel for Image		
+		JPanel imagePanel = new JPanel(); 
+		imagePanel.setBackground(Color.decode("#ddefe3"));
+		imagePanel.setPreferredSize(new Dimension(225, 225));
+		imagePanel.setLayout(new BorderLayout(0, 0));
+		
+		JLabel image = new JLabel(new ImageIcon(imgSrc)); 
+		imagePanel.add(image, BorderLayout.SOUTH);
+		
+		// Creation of panel for Donation Message
+		JPanel msgPanel = new JPanel(); 
+		msgPanel.setPreferredSize(new Dimension(225, 75)); 
+		msgPanel.setBackground(main.darkgreen);
+		msgPanel.setLayout(new BorderLayout());
+		
+		// Inserting Text to msgPanel
+		JLabel donationmsg = new JLabel(desc, SwingConstants.HORIZONTAL);
+		donationmsg.setFont(new Font("Verdana", Font.PLAIN, 12));
+		donationmsg.setForeground(main.lightgreen);
+		
+		// Adding Image and Donation Message panels to the main Donation Panel
+		msgPanel.add(donationmsg, BorderLayout.CENTER);
+		donationPanel.add(imagePanel, BorderLayout.NORTH);
+		donationPanel.add(msgPanel, BorderLayout.SOUTH);
+		
+		// For function of Button
+		donationPanel.addMouseListener(new MouseAdapter() {
+			public void mouseEntered(MouseEvent e) 
+			{ 
+				donationmsg.setFont(new Font("Verdana", Font.BOLD, 12));
+				imagePanel.setBackground(Color.decode("#ddefe3"));
+			}
+			public void mouseExited(MouseEvent e) 
+			{ 
+				donationmsg.setFont(new Font("Verdana", Font.PLAIN, 12));
+				imagePanel.setBackground(Color.decode("#ddefe3"));
+			}
+		});
+		
+		return donationPanel;
+	
+}
+
 }
