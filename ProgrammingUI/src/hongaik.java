483e4f8c3134174ea97214083071b90827b5f2f0
unknown <tohhongaik1018@gmail.com>
2022-03-21 03:39:44 +0800

    Banner and bot left

--- ProgrammingUI/src/hongaik.java
+++ ProgrammingUI/src/hongaik.java
@@ -1,36 +1,56 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.io.IOException;
 import java.text.NumberFormat;
+import java.awt.image.BufferedImage;
+import java.io.File;
 
 public class hongaik {
 	public static JPanel newPanel() { 
 		
-ImageIcon TitleIcon = new ImageIcon("ProgrammingUI/src/Beach 2.jpg");
-		
 		JPanel panel = new JPanel(); 
-		panel.setBorder(BorderFactory.createMatteBorder(4, 4, 4, 4,Color.decode("#000000")));
 		panel.setPreferredSize(new Dimension(1050, 625));
 		panel.setBackground(main.lightgreen);
-		panel.setLayout(new GridLayout(4, 1, 1, 1));
-		
-		JLabel Title = new JLabel();
-		Title.setIcon(TitleIcon);
-		Title.setText("Welcome to Life Below Water Site");
-		Title.setVerticalAlignment(JLabel.TOP);
-		Title.setHorizontalAlignment(JLabel.CENTER);
+		panel.setLayout(new BorderLayout());
 		
 		//write your code below 
-		JPanel Intro = new JPanel();
-		JPanel Selection = new JPanel();
+		//Creation of Top and Bottom Panel
+		JPanel Top = new JPanel(new BorderLayout());
+		Top.setBackground(Color.CYAN);
+		JPanel Bottom = new JPanel(new BorderLayout());
+		Bottom.setBackground(Color.decode("#FFB35C"));
+		Bottom.setPreferredSize(new Dimension(1050,425));
 		
-		//Creation of Title/Picture pane
-		Intro.setBackground(Color.decode("#09C3DB"));
-		Intro.setBounds(0,0,300,300);
-		Intro.setLayout(new FlowLayout());
-		Intro.add(Title);
-		panel.add(Intro);
-
+		//Re-scaling the image
+		BufferedImage img = null;
+		try {
+			img = ImageIO.read(new File("ProgrammingUI/src/Banner.jpg"));
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		Image dimg = img.getScaledInstance(1050, 200, Image.SCALE_SMOOTH);
+		JLabel Banner = new JLabel();
+		
+		//Creation of Top Banner
+		ImageIcon Intro = new ImageIcon(dimg);
+		Banner.setIcon(Intro);
+		Banner.setPreferredSize(new Dimension(1050, 200));
+		Top.add(Banner);
+		panel.add(Top, BorderLayout.NORTH);
+		
+		//Creation of Bottom Left Panel
+		JPanel info = new JPanel();
+		JLabel information = new JLabel();
+		information.setText("Hello World");
+        information.setFont(new Font("Open Sans Extra Bold", Font.PLAIN, 12));
+        info.setBackground(Color.decode("#1DA2D8"));
+        info.setPreferredSize(new Dimension(350, 400));
+        info.add(information, BorderLayout.NORTH);
+        panel.add(Bottom, BorderLayout.WEST);
+        
 		//write your code above
 		
 		return panel;
